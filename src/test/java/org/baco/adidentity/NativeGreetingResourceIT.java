3b9f2214524d8c5f03a1385a0b10c11672d44f83
deiby.nahuat <deiby.nahuat@galletasdonde.com>
2020-05-24 01:07:42 -0500

    Forgot to remove tests for GreetingResource service

--- src/test/java/org/baco/adidentity/NativeGreetingResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.baco.adidentity;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
-
-    // Execute the same tests but in native mode.
-}
